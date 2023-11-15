9e80b13381bc50b729678b334fe77dc84b92eea5
moniflyer <ilyamalyaev4@gmail.com>
2023-11-15 12:40:33 +0300

    saving to file added

--- script.py
+++ script.py
@@ -1,31 +1,30 @@
 import psycopg2
-from config import host, user, password, db_name
 
+def generate_etl(source_schema, source_table):
 
-try:
+    try:
+        
+        connection = psycopg2.connect(
+            host="127.0.0.1",
+            user="ilya",
+            password="123456",
+            database="postgres"
+        )
     
-    connection = psycopg2.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=db_name
-    )
-
-    connection.autocommit = True
+        connection.autocommit = True
+        
+        with connection.cursor() as cursor:
+                mapping_list = "select * from meta.attribute_mapping"
+                mapping_out_table = f"""select distinct target_schema, target_table 
+                                    from meta.attribute_mapping 
+                                    where source_schema = '{source_schema}' and source_table = '{source_table}'"""
+                cursor.execute(mapping_out_table)
+                name_schema_table = cursor.fetchall()
+                name_schema = name_schema_table[0][0]
+                name_table = name_schema_table[0][1]
+                cursor.execute(mapping_list)
+                res = cursor.fetchall()
     
-    with connection.cursor() as cursor:
-            mapping_list = "select * from meta.attribute_mapping"
-            mapping_out_table = """select distinct target_schema, target_table 
-                                from meta.attribute_mapping 
-                                where source_schema = 'source' and source_table = 'my_sample'"""
-            cursor.execute(mapping_out_table)
-            name_schema_table = cursor.fetchall()
-            name_schema = name_schema_table[0][0]
-            name_table = name_schema_table[0][1]
-            cursor.execute(mapping_list)
-            res = cursor.fetchall()
-
-            def case_change(name_schema, name_table):
                 name_scheme_table = f"{name_schema}.{name_table}"
                 my_query = 'insert into ' + name_scheme_table + '\nselect\n '
                 for elem in res:
@@ -33,14 +32,16 @@ try:
                 my_query = my_query[:-2]
                 my_query = my_query + '\n from source.my_sample'
                 cursor.execute(my_query)
+                
+                my_file = open('case_change_script/select_file.txt', 'w+')
+                my_file.write(my_query)
+                my_file.close    
+                
+    except Exception as _ex:
+        print("[INFO] Error while working with PostgreSQL:", _ex)
+    finally:
+        if connection:
+            connection.close()
+            print("[INFO] PostgreSQL connection closed")
 
-            case_change(name_schema, name_table)
-        
-
-            
-except Exception as _ex:
-    print("[INFO] Error while working with PostgreSQL:", _ex)
-finally:
-    if connection:
-        connection.close()
-        print("[INFO] PostgreSQL connection closed")
+generate_etl('source', 'my_sample')
